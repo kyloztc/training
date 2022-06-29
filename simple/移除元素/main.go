a688f7ec2708b5a983f986aa9b354545c114177f
kyloztc <3372533881@qq.com>
2022-06-29 21:52:44 +0800

    移除元素

---
+++ simple/移除元素/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func removeElement(nums []int, val int) int {
+	index := 0
+	findIndex := 0
+	numsLen := len(nums)
+	for findIndex < numsLen {
+		if nums[findIndex] != val {
+			nums[index] = nums[findIndex]
+			index += 1
+		}
+		findIndex += 1
+	}
+	fmt.Printf("nums: %v\n", nums)
+	return index + 1
+}
+
+func main() {
+	nums := []int{
+		0, 1, 2, 2, 3, 0, 4, 2,
+	}
+	val := 2
+	res := removeElement(nums, val)
+	fmt.Printf("res: %v\n", res)
+}
