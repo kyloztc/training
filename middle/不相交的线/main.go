407d607a988f480715992b9179740c23bafa48d0
kyloztc <3372533881@qq.com>
2022-11-05 21:36:49 +0800

    最长公共子序列

---
+++ middle/不相交的线/main.go
@@ -0,0 +1,28 @@
+package main
+
+// 最长公共子序列
+func maxUncrossedLines(nums1 []int, nums2 []int) int {
+	m := len(nums1)
+	n := len(nums2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if nums1[i-1] == nums2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
