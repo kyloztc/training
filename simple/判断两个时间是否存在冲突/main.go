b6ea561488436c9300e766bd34732c4275baadba
kyloztc <3372533881@qq.com>
2023-05-17 22:01:46 +0800

    判断两个时间是否存在冲突

---
+++ simple/判断两个时间是否存在冲突/main.go
@@ -0,0 +1,8 @@
+package main
+
+func haveConflict(event1 []string, event2 []string) bool {
+	return (event1[0] <= event2[1] && event1[0] >= event2[0]) || 
+	(event1[1] <= event2[1] && event1[1] >= event2[0]) || 
+	(event2[1] <= event1[1] && event2[1] >= event1[0]) || 
+	(event2[0] <= event1[1] && event2[0] >= event1[0])
+}
